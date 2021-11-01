55044f7f7f3b22effca3005c85784c29f03b7e43
Vados21 <vadim.shpilevskiy@yandex.ru>
2021-11-01 18:56:21 +0200

    backup

---
+++ yatube/posts/views.py
@@ -0,0 +1,101 @@
+from django.core.paginator import Paginator
+from django.shortcuts import get_object_or_404, redirect, render
+
+from .models import Group, Post, User
+from .forms import PostForm
+from django.contrib.auth.decorators import login_required
+
+
+def index(request):
+    #post_list = Post.objects.all().order_by('-pub_date')
+    # Если порядок сортировки определен в классе Meta модели,
+    # запрос будет выглядить так:
+    post_list = Post.objects.all()
+    # Показывать по 10 записей на странице.
+    paginator = Paginator(post_list, 10)
+
+    # Из URL извлекаем номер запрошенной страницы - это значение параметра page
+    page_number = request.GET.get('page')
+
+    # Получаем набор записей для страницы с запрошенным номером
+    page_obj = paginator.get_page(page_number)
+    # Отдаем в словаре контекста
+    context = {
+        'page_obj': page_obj,
+    }
+    return render(request, 'posts/index.html', context)
+
+
+def group_posts(request, slug):
+    group = get_object_or_404(Group, slug=slug)
+    post_list = group.posts.all()
+    paginator = Paginator(post_list, 2)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    template = 'posts/group_list.html'
+    context = {
+        'group': group,
+        'page_obj': page_obj,
+    }
+    return render(request, template, context)
+
+
+def profile(request, username):
+    user = User.objects.get(username=username)
+    post_list = Post.objects.filter(author=user)
+    paginator = Paginator(post_list, 10)
+    page_number = request.GET.get('page')
+    post_counter = paginator.count
+    page_obj = paginator.get_page(page_number)
+    context = {
+        'username': user,
+        'post_counter': post_counter,
+        'post_list': post_list,
+        'page_obj': page_obj,
+    }
+    return render(request, 'posts/profile.html', context)
+
+
+def post_detail(request, post_id):
+    template = 'posts/post_detail.html'
+    post = get_object_or_404(Post, id=post_id)
+    author_posts_count = Post.objects.filter(author_id=post.author_id).count()
+    text = get_object_or_404(Post, pk=post_id)
+    posts = Post.objects.filter(pk=post_id)
+
+    context = {
+        'posts': posts,
+        'text': text,
+        'author_posts_count': author_posts_count
+    }
+    return render(request, template, context)
+
+
+@login_required
+def post_create(request):
+    form = PostForm()
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect(f'/profile/{request.user}/')
+    context = {'form': form}
+
+    return render(request, 'posts/create_post.html', context)
+
+
+@login_required
+def post_edit(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    form = PostForm(request.POST or None, instance=post)
+    if form.is_valid():
+        form.save()
+        return redirect('posts:post_detail', post_id)
+    context = {
+        'post': post,
+        'form': form,
+        'is_edit': True,
+    }
+    return render(request, 'posts/create_post.html', context)
