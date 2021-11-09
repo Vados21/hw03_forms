9738bd2fa6c1db1da0460ff84b96b2641c2b6c0e
Vados21 <vadim.shpilevskiy@yandex.ru>
2021-11-09 17:30:16 +0200

    project_forms_3

--- yatube/users/views.py
+++ yatube/users/views.py
@@ -1,9 +1,10 @@
+from django.contrib.auth.forms import PasswordResetForm
 from django.urls import reverse_lazy
 from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import CreateView
 from django.views.generic.edit import CreateView
 
-from .forms import ContactForm, CreationForm, PasswordResetForm
+from .forms import ContactForm, CreationForm
 
 
 class SignUp(CreateView):
